8f501e09bf9aa3aa16f830943412346d8c07085b
jake871128 <319105@stu.chsc.tw>
2020-03-11 16:43:42 +0800

    Add led.cpp

--- mbed-os-test-program2/led.cpp
+++ mbed-os-test-program2/led.cpp
@@ -5,7 +5,21 @@ void Led(DigitalOut& ledpin)
 
 {
 
-   for(int i=0; i<10; ++i) { //blink for 10 times
+   for(int i=0; i<6; ++i) { //blink for 10 times
+
+       ledpin = !ledpin; // toggle led
+
+       wait(0.2f);
+
+   }
+
+}
+
+void Led2(DigitalOut& ledpin)
+
+{
+
+   for(int i=0; i<4; ++i) { //blink for 10 times
 
        ledpin = !ledpin; // toggle led
 
